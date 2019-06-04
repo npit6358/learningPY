83c0e3960c2a60ae759d98e1a8f4a504320a94cf
npit6358 <npitsakis@gmail.com>
2019-06-04 15:20:35 -0400

    prob 1 done

--- prob1.py
+++ prob1.py
@@ -1,16 +1,14 @@
-"""Problem 1 from Day01.docx"""
-
-print("Hello.  Please enter your name, then your current age\
- and I'll tell you when the government pretends you get to retire.")
-
+"""Problem 1 from Day01.docx
+   Assuming you've already had your birthday this year."""
 
 def year65(name: str, age: int) -> str:
 
     curYear = 2019
     yearsToGo = 65 - age
     bigYear = curYear + yearsToGo
-    
-    return "Hi, " + name + ".  " + "You turn 65 in the year " + bigYear + "!"
+
+
+    return "Hi, " + name + ".  " + "You turn 65 in the year " + str(bigYear) + "!"
 
 
 
