f840b6baffcd30e562cd3b5f397f4357a38de70f
npit6358 <npitsakis@gmail.com>
2019-06-05 20:29:52 -0400

    prob6usingLists.py

--- prob6usingLists.py
+++ prob6usingLists.py
@@ -9,7 +9,7 @@ def mults(limit: int) -> int:
     sum = 0
     n  = 1
     firstList = []
-    
+
     while n*3 <= limit:
         if n*3 % 15 != 0:
             firstList.append(n*3)
@@ -19,11 +19,5 @@ def mults(limit: int) -> int:
     while n*5 <= limit:
         firstList.append(n*5)
         n += 1
-
-#    for x in firstList:
-#        sum += x
-
-#    firstList.sort()
-#    print(firstList)
 
     return sum(firstList)
